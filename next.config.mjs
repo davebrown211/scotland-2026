83345941c7f389e8ea5e0ad7360acd69c571bb76
David Brown <dbrown@Davids-Mac-mini.local>
2026-05-22 11:35:40 -0500

    fix again

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["leaflet", "react-leaflet"],
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "firebasestorage.googleapis.com" },
+      { protocol: "https", hostname: "lundingolfclub.co.uk" },
+      { protocol: "https", hostname: "images.squarespace-cdn.com" },
+      { protocol: "https", hostname: "www.leven-links.com" },
+      { protocol: "https", hostname: "clubv1.blob.core.windows.net" },
+      { protocol: "https", hostname: "www.dumbarnielinks.com" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+    ],
+  },
+};
+
+export default nextConfig;
